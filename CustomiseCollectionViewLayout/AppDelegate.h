d5e2017a8c2a53914fdf0d20023b15ea36c56bb4
jupyter <jupiterli0806@gmail.com>
2017-06-19 18:12:39 +1000

    Initial Commit

---
+++ CustomiseCollectionViewLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomiseCollectionViewLayout
+//
+//  Created by jupyter on 19/6/17.
+//  Copyright © 2017 jupyter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomiseCollectionViewLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomiseCollectionViewLayout
+//
+//  Created by jupyter on 19/6/17.
+//  Copyright © 2017 jupyter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
